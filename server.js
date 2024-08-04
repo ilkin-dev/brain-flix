9c119c3e875c196e83beb806ab9a5b4fca47b89b
Ilkin Mammadzada <ilkindigo.ca@gmail.com>
2024-08-04 18:34:26 -0400

    Add: videos route and get videos data from json file

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const videoRoutes = require('./routes/videos');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use('/public', express.static('public'));
+
+app.use('/videos', videoRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
