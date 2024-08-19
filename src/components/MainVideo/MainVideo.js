9e71dcddcb48fb3e8fd3366c03034d41f953d399
Ilkin Mammadzada <ilkindigo.ca@gmail.com>
2024-08-18 22:38:17 -0400

    Update: Adjust main video player to new API

--- src/components/MainVideo/MainVideo.js
+++ src/components/MainVideo/MainVideo.js
@@ -1,25 +1,41 @@
+import React, { useState, useEffect } from 'react';
 import './MainVideo.css';
 
 const MainVideo = ({ currentVideoDetails, apiKey }) => {
+  const [isVideoPlayed, setIsVideoPlayed] = useState(false);
+
+  useEffect(() => {
+    setIsVideoPlayed(false);
+  }, [currentVideoDetails]);
+
   if (!currentVideoDetails) {
     return <div>Loading video...</div>;
   }
 
-  let videoSrc = `${currentVideoDetails.video}?api_key=${apiKey}`;
+  const videoSrc = `${currentVideoDetails.video}?api_key=${apiKey}`;
+
+  const handlePlay = () => {
+    setIsVideoPlayed(true);
+  };
+
+  const handlePause = () => {
+    setIsVideoPlayed(false);
+  };
+
   return (
     <div className="mainVideo section">
-      <div className='container'>
-        <img className='mainVideo__thumbnail' src={currentVideoDetails.image} alt='VIDEO THUMBNAIL' ></img>
-
-        <div className='videoPlayer'>
+      <div className="container">
+        <div className="videoPlayer">
           <video
             className="videoPlayer__video"
             controls
-            autoPlay
             loop
             preload="auto"
             width="100%"
             height="auto"
+            poster={!isVideoPlayed ? currentVideoDetails.image : ''}
+            onPlay={handlePlay}
+            onPause={handlePause}
           >
             <source src={videoSrc} type="video/mp4" />
           </video>
